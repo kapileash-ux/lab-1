cc752d093271ad23dea13d83a2aac1f092d239f8
kapileash-ux <kapileashkapileash83@gmail.com>
2026-04-30 14:29:21 +0530

    Add files via upload

---
+++ kapil lab 3 lw.py
@@ -0,0 +1,56 @@
+'''
+#task 16
+a=int(input("enter a number:"))
+if a==1 :
+    print("you can go out and play")
+else:
+    print("you can not go to play")
+
+#task 17
+l=int(input("Enter the length: "))
+b=int(input("enter the breath: "))
+if l==b:
+    print("it's a square")
+else:
+    print("its a rectangle")
+
+#task 21
+n=int(input("Enter a number :"))
+if n%5==0 and n%3==0:
+    print("its is multiple of both 5 and 3")
+else:
+    print("%its is multiple of both 5 and 3")
+
+#task 22
+n=int(input("enter a number:"))
+if 100<=n<=999 and n%10==0:
+    print("multiple of 10")
+else:
+    print("it is not multiple of 10")
+    
+#task 23
+n=int(input("enter a number:"))
+if 100<=n<=999 and n%2==0:
+    print("multiple of 2")
+elif 100<=n<=999 and n%5==0:
+    print("it is multiple of 5")
+elif 100<=n<=999 and n%10==0:
+    print("it is multiple of 10")
+else:
+    print("it is not multiple of 2,5,10")
+
+#task 24
+a=int(input("enter a number:"))
+b=int(input("enter a number:"))
+if a%2==0 and b%2==0:
+    print(a*b)
+else:
+    print(a+b)
+
+#task 25
+n=int(input("enter a number:"))
+if n%10==7 or n%7==0:
+    print("it is a buzz number")
+else:
+    print("it is is not a buzz number")
+'''
---
+++ lw lab2.py
@@ -0,0 +1,117 @@
+'''
+#task 1
+a=10
+b=20
+if a<b:
+    print(a)
+else:
+    print(b)
+    
+#task 2
+a=10
+b=20
+if a>b:
+    print(a)
+else:
+    print(b)
+    
+#task 3
+num=int(input("Enter number:"))
+if(num>0):
+   print("Absolute:",num)
+else:
+    print("Asolute:",(-(num)))
+
+#task 4
+num=9
+if(num%2==0):
+    print("Even Number")
+else:
+    print("odd Number")
+
+#task 5
+a=int(input("num="))
+if(a%5==0):
+    print("Multiple by 5")
+else:
+    print("not multiple by 5")
+
+#task 6
+num=20
+if(num%10==0):
+    print("Multiple by 10")
+else:
+    print("Not multiple by 10")
+    
+#task 7
+num=36
+if(num>=10 and num<=99) or (num>=-10 and num<=-99):
+    print("It is 2 digit number")
+else:
+    print("Not two digit")
+
+#task 7
+num=123
+if(num>=100 and num<=999) or (num>=-100 and num<=-999):
+    print("It is 3 digit number")
+else:
+    print("Not 3 digit")
+    
+#TASK 9
+num=110
+if(num%10==0):
+    print("It is end with 0")
+else:
+    print("not End with 0")
+    
+#TASK 10
+num=int(input("Enter the number"))
+square=num**2
+print("square root:",square)
+if(square>50):
+    print("Above 50")
+if(square<50):
+    print("Below 50")
+else:
+    print("Equal to 50")
+
+#task 11
+a = int(input("enter a number=:"))
+b = int(input("enter a number=:"))
+
+if a - b == 0:
+    print("0")
+else:
+    print("Not 0")
+
+#task 12
+marks = int(input("Enter a number:"))
+if marks >= 50:
+    print("Pass")
+else:
+    print("Fail")
+
+#task 13
+n = int(input("Enter a number:"))
+
+if n % 10 == 0:
+    print("Divisible")
+else:
+    print("Not Divisible")
+    
+#task 14
+n = int(input(Enter a number:"))
+
+a = n // 10
+b = n % 10
+
+print(a if a > b else b)
+
+#task 15
+c = int(input("Enter a number:"))
+
+if c == 1:
+    print("The exam will be easy")
+else:
+    print("The exam will be difficult")
+ '''   
