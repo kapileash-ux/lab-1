29fa39529d591136354ce7f03b91551e4d0fa589
kapileash-ux <kapileashkapileash83@gmail.com>
2026-05-24 21:49:11 +0530

    Add files via upload

---
+++ kapil1 lw.py
@@ -0,0 +1,74 @@
+'''
+#Task 1
+a="Hellow world"
+print(a)
+
+#task2
+a=10
+b=20
+print(a+b)
+print(a-b)
+print(a*b)
+print(a%b)
+print(a/b)
+
+#task3
+a=2
+print(a**0.5)
+
+#task4
+base=10
+height=20
+formula=0.5*base*height
+print(formula)
+
+#task 5
+a=10
+b=20
+print((a + b)^2)
+print((a - b)^2)
+print(a^2 - b^2)
+
+#task 6
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("Before swapping: a =", a, "b =", b)
+
+a, b = b, a
+
+print("After swapping: a =", a, "b =", b)
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("Before swapping: a =", a, "b =", b)
+
+temp = a
+a = b
+b = temp
+
+print("After swapping: a =", a, "b =", b)
+
+#task 7
+kilometers = 30
+print ("Miles:", kilometers*0.63)
+
+#task8
+Celsius = 100
+Fahrenheit = (Celsius*1.8)+32
+print ("Fahrenheit:",Fahrenheit)
+
+#task 9
+a=100
+print(a%10)
+
+#task 10
+a=111
+print(a%100)
+
+#task 11
+integer = 54623
+print ("Input an integer:", integer)
+print ("Square of 6 is:", 6**2)
+'''
